0ff789c7f42c7a77ee2224440ab030d69b0af822
mfanweb <530385371@qq.com>
2017-08-23 10:37:12 +0800

    修改滚动bug

--- src/views/music/Music.vue
+++ src/views/music/Music.vue
@@ -1,35 +1,28 @@
 <template>
     <div class="mui-page music-home">
-        
-        <mui-scroll-view class="music-container" 
-            ref="musicScrollV" 
-            name="music-scroll-v" 
-            direction="vertical" 
-            slidesPerView="auto" 
-            :scrollbar="null"
-            >
+
+        <mui-scroll-view class="music-container" ref="musicScrollV" name="music-scroll-v" direction="vertical" slidesPerView="auto" :scrollbar="null">
             <mui-scroll-view-item style="height:auto">
-            <div class="mui-container " style="overflow:initial;position:static">
-                <!-- 幻灯片 -->
-                <mui-slide v-if="slideData.length" :autoPlay='3000'>
-                    <mui-slide-item v-for="(item,index) in slideData" :key='index' :item='item'></mui-slide-item>
-                </mui-slide>
-                <!-- 首页菜单 -->
-                <music-menu></music-menu>  
-                <!-- 新歌速递 -->
-                <music-new-song :newSong="newSong"></music-new-song>
-                <!-- 热门歌单推荐 -->
-                <music-recommend :recommend="recommend"></music-recommend>
-            </div>
+                <div class="mui-container " style="overflow:initial;position:static">
+                    <!-- 幻灯片 -->
+                    <mui-slide v-if="slideData.length" :autoPlay='3000'>
+                        <mui-slide-item v-for="(item,index) in slideData" :key='index' :item='item'></mui-slide-item>
+                    </mui-slide>
+                    <!-- 首页菜单 -->
+                    <music-menu></music-menu>
+                    <!-- 新歌速递 -->
+                    <music-new-song :newSong="newSong"></music-new-song>
+                    <!-- 热门歌单推荐 -->
+                    <music-recommend :recommend="recommend"></music-recommend>
+                </div>
             </mui-scroll-view-item>
         </mui-scroll-view>
-        
-        
+
     </div>
 </template>
 
 <script>
-import {getSilde,getNewSong,getRecommend} from '@/api/music'
+import { getSilde, getNewSong, getRecommend } from '@/api/music'
 import MusicHeader from '../common/MusicHeader'
 import MusicMenu from './MusicMenu'
 import MusicNewSong from './MusicNewSong'
@@ -38,87 +31,92 @@ export default {
     name: 'Musci',
     data() {
         return {
-            slideData:[],
-            newSong:[],
-            recommend:[]
+            slideData: [],
+            newSong: [],
+            recommend: []
         }
     },
-    mounted(){
+    mounted() {
         this.loadData()
         this.loadAll()
     },
     methods: {
-        loadData(){
-           getSilde().then((res)=>{
-               this.slideData=res.data.slider
+        loadData() {
+            getSilde().then((res) => {
+                this.slideData = res.data.slider
             })
         },
-        loadAll(){
-            
-            Promise.all([getSilde(),getNewSong(),getRecommend()]).then(results=>{
-                this.slideData=results[0].data.slider
-                if(results[1].code===0){
-                    this.newSong=results[1].data.albumlist
+        loadAll() {
+
+            Promise.all([getSilde(), getNewSong(), getRecommend()]).then(results => {
+                this.slideData = results[0].data.slider
+                if (results[1].code === 0) {
+                    this.newSong = results[1].data.albumlist
                 }
-                if(results[2].code===0){
-                    this.recommend=results[2].data.list
+                if (results[2].code === 0) {
+                    this.recommend = results[2].data.list
                 }
-                this.$nextTick(()=>{
-                    setTimeout(()=>{
+                this.$nextTick(() => {
+                    setTimeout(() => {
                         //执行更新
                         this.$refs.musicScrollV.update();
                         //执行子组件的回调函数
                         this.$refs.musicScrollV.done(this.touchmove());
-                    },20)
+                    }, 20)
                 })
             })
         },
         /**
-         * 获取 musicScrollV 的 swiper对象，通过TouchStart，TouchMove，TouchEnd来获取偏移值，设置顶部搜索的显示隐藏
+         * 获取 musicScrollV 的 swiper对象，通过TouchStart，TouchMove来获取偏移值判断方向，设置顶部搜索的显示隐藏
          */
-        touchmove(){
+        touchmove() {
             // 获取 musicScrollV 的 swiper对象
-            var scroll=this.$refs.musicScrollV.getCurrentObj()
-            var vm=this;
-            scroll.on('TouchStart', function(swiper,event){
-               //获取按下的pageY值
-                var touch=event.touches[0]
-                vm.pageY1=touch.pageY;
+            var vm = this;
+            var scroll = vm.$refs.musicScrollV.getCurrentObj()
+
+            scroll.on('TouchStart', function (swiper, event) {
+                //获取按下的pageY值
+                var touch = event.touches[0]
+                vm.pageY1 = touch.pageY;
                 //通过定时器来获取滑动中translate的值，并判断顶部搜索框的隐藏显示
                 clearInterval(vm.timer);
-                vm.timer=setInterval(searchVisibel,200)
+                vm.timer = setInterval(() => {
+                    vm.searchVisibel(scroll)
+                }, 200)
             })
-            scroll.on('TouchMove', function(swiper,event){
+            scroll.on('TouchMove', function (swiper, event) {
                 //获取移动的pageY值
-                var touch=event.touches[0]
-                vm.pageY2=touch.pageY;
+                var touch = event.touches[0]
+                vm.pageY2 = touch.pageY;
                 //通过pageY1,2的值判断上滑下滑
-                if(vm.pageY2-vm.pageY1<0){
-                        vm.direction='down'
-                    }else{
-                        vm.direction='up'
-                    }
-            })
-           function searchVisibel(){
-               var translate=scroll.getWrapperTranslate()
-              if(translate<-240){
-                    vm.$store.commit('CHANGE_SEARCH_STATUS',false)
-                    if(vm.direction==='down'){
-                        vm.$store.commit('CHANGE_SEARCH_STATUS',true)
-                    }else{
-                        vm.$store.commit('CHANGE_SEARCH_STATUS',false)
-                    }
-                }else{
-                    vm.$store.commit('CHANGE_SEARCH_STATUS',false)
+                if (vm.pageY2 - vm.pageY1 < 0) {
+                    vm.direction = 'down'
+                } else {
+                    vm.direction = 'up'
                 }
-           }
-           //滚动结束，清除定时器
-           scroll.on('TransitionEnd',function(){
-               clearInterval(vm.timer);
-           })
+            })
+            //滚动结束，清除定时器
+            scroll.on('TransitionEnd', function () {
+                clearInterval(vm.timer);
+            })
+        },
+        searchVisibel(obj) {
+            var vm = this;
+            var translate = obj.getWrapperTranslate()
+
+            if (translate < -240) {
+                vm.$store.commit('CHANGE_SEARCH_STATUS', false)
+                if (vm.direction === 'down') {
+                    vm.$store.commit('CHANGE_SEARCH_STATUS', true)
+                } else {
+                    vm.$store.commit('CHANGE_SEARCH_STATUS', false)
+                }
+            } else {
+                vm.$store.commit('CHANGE_SEARCH_STATUS', false)
+            }
         }
     },
-    components:{
+    components: {
         MusicHeader,
         MusicMenu,
         MusicNewSong,
@@ -132,15 +130,17 @@ export default {
 @import '../../assets/less/variables.less';
 @import '../../assets/less/mixins.less';
 
- 
-.music-home .mui-container{
-    padding-top:(88+75-4)/@rem;
+
+.music-home .mui-container {
+    padding-top: (88+75-4)/@rem;
     overflow: initial;
 }
-.music-home{
+
+.music-home {
     background: #f4f4f4;
 }
-.music-container{
+
+.music-container {
     height: 100%;
 }
 </style>
